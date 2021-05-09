d3dad42f625292a3e73d9d0fefccd2b63ab98e87
Y-Kenza <hk_yousfi@esi.dz>
2021-05-09 16:22:43 +0100

    init repo

---
+++ app/src/main/java/com/sil1/autolibdz_onboard_computer/ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sil1.autolibdz_onboard_computer.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sil1.autolibdz_onboard_computer.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
