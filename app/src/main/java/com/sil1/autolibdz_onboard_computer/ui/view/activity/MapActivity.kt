39aa91bd9457db634899e591a2320b4be7812166
hb_chergui@esi.dz <hb_chergui@esi.dz>
2021-06-12 23:45:04 +0100

    add MapActivity

---
+++ app/src/main/java/com/sil1/autolibdz_onboard_computer/ui/view/activity/MapActivity.kt
@@ -0,0 +1,12 @@
+package com.sil1.autolibdz_onboard_computer.ui.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sil1.autolibdz_onboard_computer.R
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
